7bdb8785b5ac710e3593d92fd802664514feae42
Keith Dahlby <keith@newbo.co>
2018-08-29 09:26:42 -0500

    Use debug in error middleware

--- src/middleware/json-404.js
+++ src/middleware/json-404.js
@@ -1,7 +1,11 @@
 'use strict';
 
+// TODO: this is actually error
+
+const debug = require('debug')('app:error');
+
 export default (err, req, res, next) => {
-  console.error(err);
+  debug(err);
 
   if (req.headers['accept'] !== 'application/json') {
     next(err);
